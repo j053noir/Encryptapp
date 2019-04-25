523882cd2e5655ec7e1fc01a85cfd8e5b888ba15
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2019-04-25 11:29:53 -0500

    Generated Sign In, Sign Up and Home Components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './shared/guards/auth.guard';
+import { InSessionGuard } from './shared/guards/in-session.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'signin', component: SignInComponent, canActivate: [InSessionGuard] },
+  { path: 'signup', component: SignUpComponent, canActivate: [InSessionGuard] },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'signin' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
