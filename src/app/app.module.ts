622b10dcb4c72d8dda1ea9a900edafe266fed147
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2019-04-25 10:22:50 -0500

    Created Authentication service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AuthService } from './shared/services/auth.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
