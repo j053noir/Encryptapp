a8be6521d894043bf70d21d52f09b1f72a1f3189
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2019-04-26 12:00:08 -0500

    Added link to Sign In from Sign up. Added Max length validation to Sign up fields. Added submit to Sign up method.

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,25 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { validatePassword } from 'src/app/shared/validators/password.validator';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy {
   signUpForm: FormGroup;
   submitting = false;
 
-  constructor() {}
+  signUpSubscription: Subscription;
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      firstname: new FormControl('', Validators.required),
-      lastname: new FormControl('', Validators.required),
-      password: new FormControl('', [Validators.required, validatePassword]),
+      username: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(50),
+      ]),
+      firstname: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(50),
+      ]),
+      lastname: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(50),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(100),
+        validatePassword,
+      ]),
     });
+  }
+
+  ngOnDestroy() {
+    if (this.signUpSubscription) {
+      this.signUpSubscription.unsubscribe();
+    }
   }
 
   onSubmit() {
@@ -29,6 +53,21 @@ export class SignUpComponent implements OnInit {
 
     this.submitting = true;
 
-    console.log(this.signUpForm.value);
+    const { firstname, lastname, username, password } = this.signUpForm.value;
+
+    this.signUpSubscription = this.authService
+      .signUp(firstname, lastname, username, password)
+      .subscribe(
+        (res: any) => {
+          const message = res.message || 'Cuenta creada';
+          alert(message);
+          this.router.navigate(['/signin']);
+        },
+        err => {
+          console.log(err);
+          const message = err.message || 'Error al crear cuenta';
+          alert(message);
+        }
+      );
   }
 }
