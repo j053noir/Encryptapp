bf89133dd941c2f3f5925699f3dd1d34bce88a9d
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2019-04-26 15:53:01 -0500

    Created Users component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { UsersComponent } from './features/users/users.component';
 import { DashboardComponent } from './home/dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './shared/guards/auth.guard';
@@ -9,14 +10,16 @@ import { InSessionGuard } from './shared/guards/in-session.guard';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent,
     canActivate: [AuthGuard],
-    children: [{ path: '', component: DashboardComponent }],
+    children: [
+      { path: 'users', component: UsersComponent },
+      { path: '', component: DashboardComponent, pathMatch: 'full' },
+    ],
   },
   { path: 'signin', component: SignInComponent, canActivate: [InSessionGuard] },
   { path: 'signup', component: SignUpComponent, canActivate: [InSessionGuard] },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'signin' },
 ];
 
