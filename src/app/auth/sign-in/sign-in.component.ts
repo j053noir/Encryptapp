6bb0535024ae5ef02bdfbe3cdfa694b0d7299b25
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2019-04-26 09:47:00 -0500

    Unsubscribed on component destroy in Sign In. Added link to signup from sign in.

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent implements OnInit, OnDestroy {
   signInForm: FormGroup;
   submitting = false;
   returnUrl: string;
+
+  signInSubscription: Subscription;
 
   constructor(
     private authService: AuthService,
@@ -30,6 +33,12 @@ export class SignInComponent implements OnInit {
     });
   }
 
+  ngOnDestroy() {
+    if (this.signInSubscription) {
+      this.signInSubscription.unsubscribe();
+    }
+  }
+
   onSubmit() {
     if (!this.signInForm.valid) {
       return;
@@ -38,18 +47,20 @@ export class SignInComponent implements OnInit {
     this.submitting = true;
     const { username, password } = this.signInForm.value;
 
-    this.authService.signIn(username, password).subscribe(
-      data => {
-        if (this.returnUrl) {
-          this.router.navigate([this.returnUrl]);
-        } else {
-          this.router.navigate(['/']);
+    this.signInSubscription = this.authService
+      .signIn(username, password)
+      .subscribe(
+        data => {
+          if (this.returnUrl) {
+            this.router.navigate([this.returnUrl]);
+          } else {
+            this.router.navigate(['/']);
+          }
+        },
+        err => {
+          const message = err.message || 'error al autenticar';
+          alert(message);
         }
-      },
-      err => {
-        const message = err.message || 'error al autenticar';
-        alert(message);
-      }
-    );
+      );
   }
 }
