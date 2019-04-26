64dcb9a59bf9274f4fd017461d346660e77c7782
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2019-04-26 15:29:39 -0500

    Created Header and Dashboard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 import { InSessionGuard } from './shared/guards/in-session.guard';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+    children: [{ path: '', component: DashboardComponent }],
+  },
   { path: 'signin', component: SignInComponent, canActivate: [InSessionGuard] },
   { path: 'signup', component: SignUpComponent, canActivate: [InSessionGuard] },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
