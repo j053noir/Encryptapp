ccdae82793aa1c0bf80eb450824bf541f944bd0c
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2019-04-26 21:27:18 -0500

    Created Utils Service.

---
+++ src/app/shared/services/utils.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UtilsService {
+  constructor(private http: HttpClient) {}
+
+  cypher(form: FormData) {
+    return this.http
+      .post(`//${environment.apiUrl}/cifrado`, form)
+      .pipe(map(response => response));
+  }
+
+  decypher(form: FormData) {
+    return this.http
+      .put(`//${environment.apiUrl}/cifrado`, form)
+      .pipe(map(response => response));
+  }
+}
